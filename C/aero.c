17c4d0c0923462fe0966d15a2cafa4562903184c
Riyapatel1224 <riya.patell1204@gmail.com>
2023-04-13 17:09:26 +0530

    ..

---
+++ C/aero.c
@@ -0,0 +1,21 @@
+/*
+    Author: Riya Patel
+    DOC: 13, April 2023
+    Objective: printing hello world
+*/
+
+#include <stdio.h>
+void main()
+{
+    printf("\n*********");
+    printf("\n *        *");
+    printf("\n  *         *");
+    printf("\n******************************************************");
+    printf("\n *                                                     *");
+    printf("\n  *                                                      *");
+    printf("\n   *********************************************************");
+    printf("\n                 *         *");
+    printf("\n                *         *");
+    printf("\n               ***********");
+}
+    
--- C/hello.c
+++ C/hello.c
@@ -1,5 +1,8 @@
-
-    
+/*
+    Author: Riya Patel
+    DOC: 13, April 2023
+    Objective: printing hello world
+*/
 
 #include <stdio.h>
 void main()
