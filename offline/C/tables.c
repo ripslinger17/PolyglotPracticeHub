1ff9b415c77f705ef93d1dd67aacac1406e44220
Riyapatel1224 <riya.patell1204@gmail.com>
2023-05-04 11:15:47 +0530

    ...

---
+++ offline/C/tables.c
@@ -0,0 +1,17 @@
+/*
+    Author: Riya Patel
+    DOC: 4, may 2023
+    Objective: number loop
+*/
+
+#include <stdio.h>
+void main()
+{
+    
+    for (int i = 1; i <= 10; i++)
+    {
+        int j = 5;
+        printf("%d x %d = %d\n", j,i,j*i);
+    }
+    printf("\n");
+}
