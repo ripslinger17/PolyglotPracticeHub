071135a77f673dae6a59be2180c881ec9a7dda90
Riyapatel1224 <riya.patell1204@gmail.com>
2023-09-18 12:38:03 +0530

    ...

---
+++ python/prt.py
@@ -0,0 +1,3 @@
+print("c:\ newfolder\ bin\ temp\ python")
+print('alpha said beta "gemma is teasing me"')
+print("alpha said beta, i can't teach you because you are very intelligent")
--- python/slice.py
+++ python/slice.py
@@ -2,7 +2,7 @@ s1='ROYAL TECHNOSOFT limited'
 print(s1[0:5])
 print(s1[6:16])
 print(s1[17:24])
-print(f"{s1[0:5]}{s1[17:24]}")
+print(f"{s1[0:5]} {s1[17:24]}")
 print("\n")
 
 s2='strings are IMMUTABLE so, methods of strings cannot change the original string. Instead'
