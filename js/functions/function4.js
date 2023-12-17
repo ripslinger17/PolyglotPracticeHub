0486d7895cc6e670a411d6bb792b497b850df5b8
Riyapatel1224 <riya.patell1204@gmail.com>
2023-12-18 00:11:17 +0530

    ...

--- js/functions/function4.js
+++ js/functions/function4.js
@@ -1,15 +1,14 @@
-function getUserData(n, age, email, ph) {
-    n = prompt("Enter your name: ")
-    age = prompt("Enter your age : ")
-    email = prompt("Enter you email: ")
-    ph = prompt("Enter your phone number : ")
-    return n, age, email, ph;
+function getUserData() {
+    var n = prompt("Enter your name: ");
+    var age = prompt("Enter your age: ");
+    var email = prompt("Enter your email: ");
+    var ph = prompt("Enter your phone number: ");
+    return { n, age, email, ph };
 }
 
 function displayUserData(n, age, email, ph) {
-    alert("Name : " + n + "\nAge: " + age + "\nEmail: " + email + "\nPhone NUmber: " + ph);
+    alert("Name: " + n + "\nAge: " + age + "\nEmail: " + email + "\nPhone Number: " + ph);
 }
 
-var n, age, email, ph
-n, age, email, ph = getUserData(n, age, email, ph);
-displayUserData(n, age, email, ph);
+var userData = getUserData();
+displayUserData(userData.n, userData.age, userData.email, userData.ph);
