2c8f66ae2ebfc172eeff1324e928e44ad84eae48
Riyapatel1224 <riya.patell1204@gmail.com>
2024-01-01 19:59:27 +0530

    ...

---
+++ js/json/json2.js
@@ -0,0 +1,34 @@
+var property = {
+    BHK: 4,
+    area: 'pdpu',
+    valuation: "3cr",
+    total_bungalow: 10,
+    floor_area: "500 sqft",
+    no_of_balconies: 3,
+    type: "Residential",
+}
+comsole.log(property)
+
+
+var exam = {
+    college_name: "XYZ College of Engineering",
+    branch: 'IT',
+    name: 'Mathematics',
+    paper: 'A-level',
+    date: "12 jan",
+    time: "9.30 am"
+}
+
+comsole.log(exam)
+
+
+var college = {
+    name: "XYZ College of Engineering",
+    address: "abc",
+    total_branches: 6,
+    courses: ['BTech', 'MCA', 'Masters'],
+    website: "www.xyzcollege.com",
+    tier: "3rd tier"
+}
+
+comsole.log(college)
