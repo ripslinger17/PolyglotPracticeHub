6332666c3be6dc388c007c68bb5a08623f26b43c
Riyapatel1224 <riya.patell1204@gmail.com>
2024-01-26 16:01:08 +0530

    ...

---
+++ js/json/jsonArray/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < students.length; i++)
+// {
+//     console.log(students[i].name)
+// }
+
+
+// students.forEach(s => {
+//     console.log(s.name)
+//     console.log(s.age)
+// })
+
+// var sname = students.map(sn => {
+//     return sn.name
+// })
+// console.log(sname)
