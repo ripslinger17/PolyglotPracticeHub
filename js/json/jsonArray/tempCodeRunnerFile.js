8e7ae94006901ea5199880814e7ec8ec728ab54c
Riyapatel1224 <riya.patell1204@gmail.com>
2024-01-29 19:55:00 +0530

    ...

--- js/json/jsonArray/tempCodeRunnerFile.js
+++ js/json/jsonArray/tempCodeRunnerFile.js
@@ -1,15 +1,10 @@
-for (let i = 0; i < students.length; i++)
-// {
-//     console.log(students[i].name)
-// }
+const cssFemaleStudents = data.find((d) => d.course == "CSS").students.filter((stu) => stu.gender == "female")
+// console.log(cssFemaleStudents)
 
 
-// students.forEach(s => {
-//     console.log(s.name)
-//     console.log(s.age)
-// })
+// const totalCssMarks = data.find((d) => d.course == "CSS").students.reduce((total, stu) => total + stu.marks, 0)
+// console.log(totalCssMarks)
 
-// var sname = students.map(sn => {
-//     return sn.name
-// })
-// console.log(sname)
+
+// const htmlstufil = data.find((d) => d.course == "HTML").students.filter((stu) => stu.age < 22 && stu.gender == "male")
+// console.log(htmlstufil)
